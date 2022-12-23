8789b77be8bb1bae054eea78e0f0b23b2284a1a4
Abaevsk94 <abaevsk@gmail.com>
2022-12-23 15:32:26 +0300

    1 task

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,30 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int element : arr) {
+            sum = sum + element;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+
     }
 }
