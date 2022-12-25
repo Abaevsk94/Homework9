b6fed2a909d07d2279313b4bc564ec13c49d66e8
Abaevsk94 <abaevsk@gmail.com>
2022-12-25 20:20:24 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
+        task4();
     }
 
     public static int[] generateRandomArray() {
@@ -21,10 +23,44 @@ public class Main {
             sum = sum + element;
         }
         System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+
     }
 
     public static void task2() {
         System.out.println("Задача 2");
+        int[] arr = generateRandomArray();
+        int max = arr[0];
+        int min = arr[0];
+        for (int element : arr) {
+            if (element < min) {
+                min = element;
+            }
+            if (element > max) {
+                max = element;
+            }
+        }
+        System.out.println("Минимальное значение равно " + min);
+        System.out.println("Максимольное значение равно " + max);
+    }
 
+    public static void task3() {
+        System.out.println("Задача 3");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        int average = 0;
+        int daysInMonth = 30;
+        for (int element : arr) {
+            sum += element;
+            average = sum / daysInMonth;
+        }
+        System.out.println("Средняя сумма трат за месяц составила " + average + " рублей");
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
